7e4cf54bc4c64bfe14ce2e56f48fef3dd39d6b7f
Ilana Beer <IlanaMBeer@gmail.com>
2013-03-11 22:35:31 -0400

    WORKING COPY - gets user id from session
    user id can be displayed on the page

--- application/views/members_area.php
+++ application/views/members_area.php
@@ -6,7 +6,7 @@
 	<?php 
 		echo "Welcome to ".$this->session->userdata('username')."'s profile"; 
 		$id = $this->session->userdata('userid');
-			echo "<br />This user's id is: " . $id[0]->m_id;
+		echo "<br />This user's id is: " . $id[0]->m_id . "<br />";
 	?>
 	
 	<br />
