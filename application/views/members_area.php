f489f34b86fca4f14f7d2a2e0048c386bf376dee
Michael MacPherson <tothemac@gmail.com>
2013-03-14 10:47:57 -0400

    conflict?

--- application/views/members_area.php
+++ application/views/members_area.php
@@ -1,12 +1,22 @@
+<?php include_once ('includes/sort.php'); ?>
 <div class="bgWrapper">
         <section class="mainWrapper">
         	<div class="row">
             <main>
             
+<<<<<<< HEAD
 	<?php echo "Welcome to ".$this->session->userdata('username')."'s profile"; 
 		print_r($this->session->all_userdata());
 		
 	?>
+=======
+	<?php 
+		echo "Welcome to ".$this->session->userdata('username')."'s profile";
+	?>
+
+	<?php require_once ('includes/list.php'); ?>
+            
+>>>>>>> ilanaBranch
 	
 	<br />
 
