a868524be0a90a27500b7a81250587c3a9af2742
Anja <anja.gusev@gmail.com>
2013-03-25 11:41:24 -0400

    changing membership model user to members

--- application/models/membership_model.php
+++ application/models/membership_model.php
@@ -2,8 +2,8 @@
 class Membership_model extends CI_Model {
 	
 	function validate(){
-		$this->db->where('username', $this->input->post('username'));
-		$this->db->where('password', md5($this->input->post('password')));
+		$this->db->where('m_username', $this->input->post('username'));
+		$this->db->where('m_password', md5($this->input->post('password')));
 		$query = $this->db->get('members');
 		
 		
@@ -31,20 +31,21 @@ class Membership_model extends CI_Model {
 		$new_member_insert_data = array (
 		'first_name' => $this->input->post('first_name'),
 		'last_name' => $this->input->post('last_name'),
-		'email' => $this->input->post('email'),
-		'username' => $this->input->post('username'),
+		'm_email' => $this->input->post('email'),
+		'm_username' => $this->input->post('username'),
+		'm_sex' => $this->input->post('username'),
 		//we run the md5 function so we can store 32 bit hash in our database
-		'password' => md5($this->input->post('password'))
+		'm_password' => md5($this->input->post('password'))
 		);
 		//doing our insert
-		$insert = $this->db->insert('users', $new_member_insert_data);
+		$insert = $this->db->insert('members', $new_member_insert_data);
 		return $insert;
 		
 	}
 	
 	function check_if_username_exists($username) {
 	$this->db->where('username', $username);
-	$result = $this->db->get('users');
+	$result = $this->db->get('members');
 	
 	if($result->num_rows() > 0 ){
 		
@@ -60,7 +61,7 @@ class Membership_model extends CI_Model {
 	
 	function check_if_email_exists($email) {
 		$this->db->where('email', $email);
-		$result = $this->db->get('users');
+		$result = $this->db->get('members');
 		
 		if($result->num_rows() > 0 ){
 		
