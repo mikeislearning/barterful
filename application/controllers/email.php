4e2247a5f83596e9a88e24040144db996b181dfa
Anja <anja.gusev@gmail.com>
2013-03-25 11:20:05 -0400

    I don't want to commit this

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 /* SEND EMAIL WITH GMAIL */
@@ -93,3 +94,76 @@ function __construct()
 		}
 	}
 }
+=======
+<?php
+
+/* SEND EMAIL WITH GMAIL */
+
+class Email extends CI_Controller
+{
+	
+	
+function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function index()
+	{
+		$this->load->view('landingpage');//NOT ILANA BEER!
+		//YOU ARE A MONKEYS UNCLE
+		//this is my branch
+	}
+	
+	function send()
+	{
+		//$echo 'hello from send'; die();
+		
+		$this->load->library('form_validation');
+		
+		//can set 3 parameters: field name, error message, validation rules separated by |'s
+		
+		
+		$this->form_validation->set_rules('email','Email','trim|required|valid_email');
+		$this->form_validation->set_rules('message','Message','required');
+		//id name, name it prints out, the rules
+		
+		if($this->form_validation->run()==FALSE){
+			$this->load->view('landingpage');		
+		}
+		
+		else
+		{	
+			//validation passed
+			
+			$email = $this->input->post('email');
+			$message = $this->input->post('message');
+			$this->load->library('email');
+			
+			$this->email->set_newline("\r\n");//prevents an error in CI for sending an email
+			
+			$this->email->from($email);
+			$this->email->to('barterful@gmail.com','Team Barterful');
+			$this->email->subject('A Barterful fan');
+			$this->email->message($message);
+			
+			//$path = $this->config->item('server_root');//creates a proper both on how you'd find a newsletter
+
+			if($this->email->send())
+			{	
+				
+				$this->load->view('index');	
+				echo 'Send is successful!';
+				//$this->load->view('signup_confirmation_view');
+			}
+			else
+			{
+				$this->load->view('index');	
+				echo 'Send failed. Sorry :( ' ;
+				show_error($this->email->print_debugger());
+			}
+		
+		}
+	}
+}
+>>>>>>> this is the test branch
