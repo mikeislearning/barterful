1a9ea06d7b7ebd219a0acc3c8d899f397c829765
Anja <anja.gusev@gmail.com>
2013-04-20 13:05:07 -0400

    i think it is all synced?

--- application/views/profile_form.php
+++ application/views/profile_form.php
@@ -1,63 +1,51 @@
-<?php if($member) foreach ($member as $m):?>
-<?php if($profile) foreach ($profile as $p):?>
+
 <div class="bgWrapper">
         <section class="mainWrapper">
         	<div class="row">
             <main>
-            
-<h1>Update Your Profile</h1>
+<?php 
+//if the profile infromation is set load the profile view or else load the rest
+if(isset($profile)){
 
-
+//if an error is set echo it
+if(isset($error)){echo $error; }
+//if the profile is updated, echo this
+if(isset($profile_updated)) { ?>
+<h3><?php echo $profile_updated; }?></h3>
+<?php $this->load->view('includes/view_profile.php'); ?>
 
 
 <?php
-
-
-echo '<img src="../../uploads/' .$p->p_img .'"/>' . '<br/>';
+//echo '<img src="../../uploads/' .$p->p_img .'"/>' . '<br/>';
 ?>
 
-<?php echo form_open_multipart('upload/do_upload_profile');?>
+<?php 
+
+//ANJA ATTEMPT APRIL 13
+/*	echo form_open_multipart('upload'); //should go to index...
+	echo form_upload('userfile'); //expects userfile by default
+	ec-echo form_close();*/
+?>
+<!-- ANJA COMMENTED OUT APRIL 13TH TO TRY NEW UPLOAD -->
+
+<?php 
+}
+else { 
+if(isset($error)){echo $error; }
+//open the upload form
+echo form_open_multipart('upload/do_upload_profile');?>
 Upload a new image: 
 <input type="file" name="userfile" size="20" />
-				<br /><br />
+<?php echo form_submit('upload', 'Upload');
+echo form_close();
+?>
+<!--<input type="upload" value="upload" />   
 
-<input type="submit" value="upload" />
+</form>-->
 
 <?php echo anchor('members/changePassword','Change Password', 'class="button1"'); ?>
 
-</form>
 <?php
-
-echo "Username: " .$this->session->userdata('username') ."<br/>";
-if(!empty($p->p_fname)){
-	echo "First Name: " .$p->p_fname ."<br/>";
-};
-if(!empty($p->p_fname)){
-echo "Last: Name: " .$p->p_lname ."<br/>";
-};
-
-if(!empty($p->p_fname)){
-echo "Gender: " . $m->m_sex . "<br/>";
-};
-
-/*foreach ($profile as $p)
-{ 
-echo "Username: " .$this->session->userdata('username') ."<br/>";
-echo "First Name: ".$p->p_fname . "<br/>";
-echo "Last Name: ".$p->p_lname . "<br/>";
-}*/
-
-
-/*foreach ($query->result() as $row)
-{
-   echo $row->p_id;
-   echo $row->p_fname;
-   echo $row->p_lname;
-}
-*/
-//print_r($profile);
-
-//echo $profile->p_id;
 
 echo form_open('members/updateProfile');
 echo form_input('first_name', set_value('first_name', ''),'placeholder="First Name"');
@@ -65,8 +53,11 @@ echo form_input('last_name', set_value('last_name', ''),'placeholder="Last Name"
 echo form_submit('submit', 'Update Profile');
 echo validation_errors('<p class="error">');
 echo form_close();
-	
 ?>
+
+ <?php
+};?>
+
 
             
             </main>
@@ -74,15 +65,3 @@ echo form_close();
 		</section><!-- end section thing -->
         </div> <!-- end bgWrapper from mainpage -->
         
- <?php endforeach; 
-	 endforeach; ?>
-	 
-	 
-<?php if(!$member) echo "There is no one matching that description!"; ?>
-
-
-<!--HOW DO I MAKE IT DO WHAT IT DOES WHEN IT REFRESHES-->
-<?php if(!$profile) echo "error error";
-
-	
-?>
