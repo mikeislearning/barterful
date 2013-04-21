804fa8935a44fa7b7c3fcca530a5a74d1af50fa5
Anja <anja.gusev@gmail.com>
2013-04-21 13:49:43 -0400

    fuck you mike

--- application/views/signup_form.php
+++ application/views/signup_form.php
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-<div class="bgWrapper">
-        <section class="mainWrapper">
-        	<div class="row">
-            <main>
-            
-<h1>Create an Account!</h1>
-<?php
-echo form_open('login/create_member');?>
-<div id="username">
-	<label for="username">Username </label>
-<? echo form_input('username', set_value('username', ''),'placeholder="Username"'); ?>
-
-</div>
-
-<div id="email">
-	<label for="email">Email Address </label>
-<? echo form_input('email', set_value('email', ''),'placeholder="Email Address", '); ?>
-</div>
-<div id="password">
-	<label for="password">Password </label>
-<? echo form_password('password', '', 'placeholder="Password" class="password"'); ?>
-</div>
-<div id="password_confirm">
-	<label for="password_confirm">Confirm Password </label>
-<? echo form_password('password_confirm', '', 'placeholder="Confirm Password" class="password_confirm"');
-echo form_submit('submit', 'Create Account');?>
-</div>
-<?php
-echo validation_errors('<p class="error">');
-echo form_close();
-	
-?>
-
-            
-            </main>
-            </div><!-- end div with class row from mainpage -->
-		</section><!-- end section thing -->
-        </div> <!-- end bgWrapper from mainpage -->
- 
- <script type="text/javascript">
-
-//$("#email").hide().before("some shit");
-
-
-
-
-
- </script>
-=======
 <div class="bgWrapper">
         <section class="mainWrapper">
         	<div class="row">
@@ -74,4 +24,3 @@ echo form_close();
 </div><!-- end div with class row from mainpage -->
 </section><!-- end section thing -->
 </div> <!-- end bgWrapper from mainpage -->
->>>>>>> 2cb8a308a536e9f41afa31f59f5591c4452d26e1
