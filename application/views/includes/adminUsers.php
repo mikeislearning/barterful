80cf9e30f89a10bad7ac2f9b2d505148c6d45f78
Ilana Beer <IlanaMBeer@gmail.com>
2013-04-24 19:36:32 -0400

    more admin

--- application/views/includes/adminUsers.php
+++ application/views/includes/adminUsers.php
@@ -12,7 +12,7 @@
 
 <?php if($row) foreach($row as $r): ?>
 	<tr>
-		<td><input type="checkbox" checked='<?=$r->m_active ?>' class="actives" id='<?=$r->m_id ?>' /></td>
+		<td><input type="checkbox" <?php if($r->m_active == true) echo "checked" ?> class="actives" id='<?=$r->m_id ?>' /></td>
 		<td><?=$r->m_username ?></td>
 		<td><?=$r->m_email ?></td>
 		<td><?=$r->m_sex ?></td>
@@ -35,7 +35,22 @@
 
 		$('.actives').change(function()
 		{
-			alert(this.val());
+			var id = ($(this).attr("id"));
+			var status = this.checked;
+
+			//determine which call to used based on whether user is logged in or not
+			ext_url = 'index.php/superuser/memActive';
+
+			//base_url us a php function to get to the root of the site, then add the extended url
+			var send_url = '<?=base_url()?>' + ext_url;
+
+			//send the variables through
+			$.post(send_url, { id:id, status:status }).done(function(msg){
+					$('#manageContent').html(msg);
+                }).fail(function(){
+                	if(!$('#result').length)
+                	$('#manageContent').prepend("<span id='result' style='color:red;margin:0 10px;'>Unable to change status!</span>");
+                });
 		})
 	})
 </script>
